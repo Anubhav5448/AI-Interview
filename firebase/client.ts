39e77ccb0238ae291203b111c9d5d9255659aeb2
Anubhav-workspace <Anubhavyadav0606@gmail.com>
2025-04-13 18:27:09 +0530

    protective keys

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,14 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMp6EiSYz9OCLcgvKZJ5kGriRINuszYzI",
-  authDomain: "prepwise-275f3.firebaseapp.com",
-  projectId: "prepwise-275f3",
-  storageBucket: "prepwise-275f3.firebasestorage.app",
-  messagingSenderId: "1066024584350",
-  appId: "1:1066024584350:web:2cf675799920812aa1a78f",
-  measurementId: "G-SVRXBCS8H3"
+
+
+apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
